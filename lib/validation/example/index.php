8a2b3acdc0f84edf090fb35b3078e65d6991654c
Yinan Bao <byn9826@gmail.com>
2017-02-06 22:41:16 -0500

    update valid lib

---
+++ lib/validation/example/index.php
@@ -0,0 +1,126 @@
+<?php
+    //import fanta valid lib
+    include_once("../fanta_valid.php");
+
+    // Initialise variables to hold form field data
+    // ---------------------------------
+    $name = "";
+    $email = "";
+    $gender = "";
+    $phoneNumber = "";
+    $location = "";
+    $isInternational = "";
+    $message = "";
+
+    // Perform validation only if form has being submitted
+    // ---------------------------------------------------
+
+    if(isset($_POST['send-message'])) {
+        $name = $_POST['txt_name'];
+        $email = $_POST['txt_email'];
+        $gender = $_POST['gender'];
+        $phoneNumber = $_POST['txt_phone'];
+        $message = $_POST['txt_message'];
+
+        //--- Form fields validation
+        /*
+        validateName($name, $nameError);
+        validateEmail($email, $emailError);
+        validatePhoneNumber($phoneNumber, $phoneNumError);
+        validateDropDownSelection($location, "NONE", $locationError);
+        validateMessage($message, $messageError);
+        */
+        // Perform validations using fanta_valid library
+        // Validate Name
+        if(Fanta_Valid::isNullOrEmpty($name)) {
+            $nameError = "Please enter a name";
+        }
+
+        // Validate Email Address
+        if(Fanta_Valid::isNullOrEmpty($email)) {
+            $emailError = "Please enter an email address";
+        } elseif (!Fanta_Valid::isEmailValid(trim($email))) {
+            $emailError = "Please enter a valid email address";
+        }
+
+        // Validate Gender
+        if(Fanta_Valid::isNullOrEmpty($gender)) {
+            $genderError = "Please indicate your gender";
+        } elseif (!Fanta_Valid::isGenderValid($gender)) {
+            $genderError = "Please don't hack our form";
+        }
+
+        // Validate Phone Number
+        if(Fanta_Valid::isNullOrEmpty($phoneNumber)) {
+            $phoneNumError = "Please enter a valid phone number";
+        } elseif(!Fanta_Valid::isPhoneNumValid($phoneNumber)) {
+            $phoneNumError = "The phone number format is wrong";
+        }
+
+        // Validate User Message
+        if(Fanta_Valid::isNullOrEmpty($message)) {
+            $messageError = "Please enter a message";
+        } elseif (!Fanta_Valid::isAboveMinLength($message, 10)) {
+            $messageError = "Must be more than 10 characters";
+        } elseif (!Fanta_Valid::isBelowMaxLength($message, 20)) {
+            $messageError = "Should be less than 20 characters";
+        }
+
+        // If form is valid, perform logic after form submission
+        if(!isset($nameError) && !isset($emailError) && !isset($genderError) && !isset($phoneNumError) && !isset($messageError)) {
+            $success = true;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Validation Library Example</title>
+		<meta name="description" content="Example page for canada-parks validation lib" />
+		<link rel="stylesheet" type="text/css" href="content.css">
+	</head>
+	<body>
+        <main class="main">
+            <div class="wrapper">
+                <div id="main-success" <?php if(!isset($success)) echo "class='hide'"; ?> >
+                    Success!
+                </div>
+                <h1>Example Page for fanta_Valid Lib</h1>
+                <p>Please take some time to fill out this short survey on Web Development Program. This short survey is an exercise from the HTTP 5202 Web Application Development 2 lab.</p>
+                <form name="frmSurvey" action="index.php" method="post" class="survey-from">
+                    <div>
+                        <div><label for="txt_name" class="label">Your Name<span class="field-required">&nbsp;*</span></label></div>
+                        <div><input type="text" id="txt_name" name="txt_name" value="<?php echo $name ?>" placeholder="Enter your name here ..." class="form-input" /></div>
+                        <div class="field-required"><?php if(isset($nameError)) {echo $nameError;} ?></div>
+                    </div>
+                    <div>
+                        <div><label for="txt_email" class="label">Email Address<span class="field-required">&nbsp;*</span></label></div>
+                        <div><input type="text" id="txt_email" name="txt_email" value="<?php echo $email ?>" placeholder="Enter your email here ..." class="form-input" /></div>
+                        <div class="field-required"><?php if(isset($emailError)) {echo $emailError;} ?></div>
+                    </div>
+                    <fieldset>
+                        <legend class="label">Gender</legend>
+                        <input type="radio" id="M" value="Male" name="gender" <?php if($gender === 'Male') {echo 'checked';} ?> /><label for="M">Male</label>
+                        <input type="radio" id="F" value="Female" name="gender" <?php if($gender === 'Female') {echo 'checked';} ?> /><label for="F">Female</label>
+                        <div class="field-required"><?php if(isset($genderError)) {echo $genderError;} ?></div>
+                    </fieldset>
+                    <div>
+                        <div><label for="txt_phone" class="label">Phone Number</label></div>
+                        <div><input type="text" id="txt_phone" name="txt_phone" value="<?php echo $phoneNumber ?>" placeholder="647 685 9999" class="form-input" /></div>
+                        <div class="field-required"><?php if(isset($phoneNumError)) {echo $phoneNumError;} ?></div>
+                    </div>
+                    <div>
+                        <div><label for="txt_message" class="label">Why do you like Web Development? Must be between 10 - 20 characters<span class="field-required">&nbsp;*</span></label></div>
+                        <div><textarea id="txt_message" name="txt_message" placeholder="Enter your message here ..." rows="5" class="form-input"><?php echo $message ?></textarea></div>
+                        <div class="field-required"><?php if(isset($messageError)) {echo $messageError;} ?></div>
+                    </div>
+                    <div>
+                        <button class="button" type="submit" name="send-message">Submit Survey</button>
+                    </div>
+                </form>
+            </div>
+        </main>
+	</body>
+</html>
---
+++ lib/validation/fanta_valid.php
@@ -0,0 +1,47 @@
+<?php
+//Validation Lib created by Fantastic5 in Canada Parks Project
+//Author: Irfaan, Sam, Duc, Navpreet, BAO
+class Fanta_Valid
+{
+    // Constant declaration
+    // ---------------------
+    const PHONE_REGEX = "/^[0-9]{3}[ |-]?[0-9]{3}[ |-]?[0-9]{4}$/";
+
+    // Public Static Functions
+    // -----------------------
+    // Function to validate is string is not null or empty
+    public static function isNullOrEmpty($value) {
+        $trim_value = trim($value);
+        return !isset($value) || $trim_value === "";
+    }
+
+    // Function to validate if email address is in a valid format
+    public static function isEmailValid($value) {
+        return filter_var(trim($value), FILTER_VALIDATE_EMAIL);
+    }
+
+    // Function to validate if gender value is male or female
+    public static function isGenderValid($value) {
+        if(strcasecmp($value, "male") == 0 || strcasecmp($value, "female") == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Function to validate if phone number is in acceptable formats
+    public static function isPhoneNumValid($value) {
+        return preg_match(self::PHONE_REGEX, trim($value));
+    }
+
+    // Function to test if string isn't below minimum length
+    public static function isAboveMinLength($value, $minLength) {
+        return strlen($value) >= $minLength;
+    }
+
+    // Function to test if string isn't above maximum length
+    public static function isBelowMaxLength($value, $maxLength) {
+        return strlen($value) <= $maxLength;
+    }
+
+}
