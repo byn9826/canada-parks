c71ec2efc11ce83387d247969a7012c496565485
iauhammad <irfaan1213@gmail.com>
2017-02-14 22:24:27 -0500

    Profile settings page layout. Addition of static database connection method. Footprints and Wishlist layout. Test with profile picture for user (still has minor bugs).

---
+++ profile/change_pic.php
@@ -0,0 +1,116 @@
+<?php
+require_once ('../lib/myLocalhostDB.php');
+
+$post = isset($_POST) ? $_POST: array();
+switch($post['action']) {
+    case 'save' :
+        saveProfilePicTmp();
+        break;
+    default:
+        changeProfilePic();
+        break;
+}
+/* Function to change profile picture */
+function changeProfilePic() {
+    $post = isset($_POST) ? $_POST : array();
+    $max_width = "500";
+    $userId = isset($post['hdn-profile-id']) ? intval($post['hdn-profile-id']) : 0;
+    $path = '../static/img/profile/users';
+    $valid_formats = array("jpg", "png", "gif", "bmp","jpeg", "JPG", "PNG", "GIF", "BMP", "JPEG");
+    $name = $_FILES['profile-pic']['name'];
+    $size = $_FILES['profile-pic']['size'];
+    if(strlen($name)) {
+        list($txt, $ext) = explode(".", $name);
+        if(in_array($ext,$valid_formats)) {
+            if($size<(2048*2048)) {
+                $actual_image_name = 'avatar' .'_'.$userId .'.'.$ext;
+                $filePath = $path .'/'.$actual_image_name;
+                $tmp = $_FILES['profile-pic']['tmp_name'];
+                if(move_uploaded_file($tmp, $filePath)) {
+                    $width = getWidth($filePath);
+                    $height = getHeight($filePath);
+                    //Scale the image if it is greater than the width set above
+                    if ($width > $max_width){
+                        $scale = $max_width/$width;
+                        $uploaded = resizeImage($filePath,$width,$height,$scale);
+                    } else {
+                        $scale = 1;
+                        $uploaded = resizeImage($filePath,$width,$height,$scale);
+                    }
+//                    $res = saveProfilePic(array(
+//                        'userId' => isset($userId) ? intval($userId) : 0,
+//                        'avatar' => isset($actual_image_name) ? $actual_image_name : '',
+//                    ));
+                    echo "<img id='photo' file-name='".$actual_image_name."' class='' src='".$filePath.'?'.time()."' class='preview'/>";
+                }
+                else
+                    echo "failed";
+            }
+            else
+                echo "Image file size max 1 MB";
+        }
+        else
+            echo "Invalid file format..";
+    }
+    else
+        echo "Please select image..!";
+    exit;
+}
+/* Function to handle save profile pic */
+function saveProfilePic($options){
+    $objConnection = myLocalhostDB::getConnection();
+    $query = 'INSERT INTO dinosaurs (name, color) VALUES(:userId, :url) WHERE NOT EXISTS(SELECT 1 FROM dinosaurs WHERE name = :userId)';
+    $statement = $objConnection->prepare($query);
+    $statement->bindValue(':userId', $options['userId']);
+    $statement->bindValue(':url', $options['avatar']);
+    $statement->execute();
+}
+
+/* Function to update image */
+function saveProfilePicTmp() {
+    $post = isset($_POST) ? $_POST: array();
+    $userId = isset($post['id']) ? intval($post['id']) : 0;
+    $path ='\\images\tmp';
+    $t_width = 300; // Maximum thumbnail width
+    $t_height = 300;    // Maximum thumbnail height
+    if(isset($_POST['t']) and $_POST['t'] == "ajax") {
+        extract($_POST);
+        $imagePath = '../static/img/profile/users/'.$_POST['image_name'];
+        $ratio = ($t_width/$w1);
+        $nw = ceil($w1 * $ratio);
+        $nh = ceil($h1 * $ratio);
+        $nimg = imagecreatetruecolor($nw,$nh);
+        $im_src = imagecreatefromjpeg($imagePath);
+        imagecopyresampled($nimg,$im_src,0,0,$x1,$y1,$nw,$nh,$w1,$h1);
+        imagejpeg($nimg,$imagePath,90);
+        $res = saveProfilePic(array(
+            'userId' => isset($userId) ? intval($userId) : 0,
+            'avatar' => isset($actual_image_name) ? $actual_image_name : '',
+        ));
+    }
+    echo $imagePath.'?'.time();;
+    exit(0);
+}
+/* Function  to resize image */
+function resizeImage($image,$width,$height,$scale) {
+    $newImageWidth = ceil($width * $scale);
+    $newImageHeight = ceil($height * $scale);
+    $newImage = imagecreatetruecolor($newImageWidth,$newImageHeight);
+    $source = imagecreatefromjpeg($image);
+    imagecopyresampled($newImage,$source,0,0,0,0,$newImageWidth,$newImageHeight,$width,$height);
+    imagejpeg($newImage,$image,90);
+    chmod($image, 0777);
+    return $image;
+}
+/*  Function to get image height. */
+function getHeight($image) {
+    $sizes = getimagesize($image);
+    $height = $sizes[1];
+    return $height;
+}
+/* Function to get image width */
+function getWidth($image) {
+    $sizes = getimagesize($image);
+    $width = $sizes[0];
+    return $width;
+}
--- profile/index.php
+++ profile/index.php
@@ -2,13 +2,38 @@
 <html lang="en">
     <head>
         <?php
+            require_once ("../lib/DatabaseAccess.php");
+            require_once '../lib/park.php';
+            require_once '../lib/ParkRepository.php';
+
+            // TODO: Remove after test
+            require_once ("../lib/myLocalhostDB.php");
+            $profilePicURL = myLocalhostDB::getProfilePicture(666);
+
+            $parkRepository = new ParkRepository();
+            $lstParks = $parkRepository->getParks(null);
+
+            // Test SQL connection
+        //            $objConnection = DatabaseAccess::getConnection();
+        //            $query = 'SELECT * FROM wishlist';
+        //            $statement = $objConnection->prepare($query);
+        //            $statement->execute();
+        //            $lstWishlist = $statement->fetchAll(PDO::FETCH_ASSOC);
+        //            var_dump($lstWishlist);
+        //            foreach ($lstWishlist as $row) {
+        //                echo $row['wish_id'] . " | " . $row['user_id'] . " | " .
+        //                     $row['park_id'] . " | " . date('F d, Y', strtotime($row['added_on'])) . "<br />";
+        //            }
+
 			include_once "../templates/meta.php";
 		?>
 		<meta name="author" content="Irfaan">
 		<title>Profile Page | Marvel Canada</title>
         <link rel="stylesheet" type="text/css" href="../static/css/profile.css" />
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
     </head>
     <body>
         <div class="container-fluid">
@@ -23,14 +48,32 @@
                     <div class="user-details col col-md-3">
                         <!-- Profile Avatar Picture -->
                         <div class="avatar">
-                            <a href="#" title="Change your avatar">
-                                <img src="../static/img/users/profile/1.png" alt="User's avatar picture" />
+                            <a href="settings.php" title="Go to profile settings">
+                                <img id="profile_picture"
+                                     data-src="<?php
+                                     if(isset($profilePicURL)) {
+                                         echo "../static/img/profile/users/" . $profilePicURL;
+                                     } else {
+                                         echo "../static/img/profile/users/custom/default.png";
+                                     }
+                                     ?>"
+                                     data-holder-rendered="true"
+                                     src="<?php
+                                     if(isset($profilePicURL)) {
+                                         echo "../static/img/profile/users/" . $profilePicURL;
+                                     } else {
+                                         echo "../static/img/profile/users/custom/default.png";
+                                     }
+                                     ?>"
+                                     alt="User's avatar or profile picture" />
                                 <!-- Confirm with team about circle image? -->
                             </a>
                         </div>
 
                         <!-- Profile Name -->
-                        <h1 class="name"><span>Irfaan Auhammad</span></h1>
+                        <h1 class="name">
+                            <span><a href="settings.php" title="Change profile settings">Irfaan Auhammad</a></span>
+                        </h1>
 
                         <!-- User Personal Details -->
                         <div class="user-info">
@@ -53,17 +96,17 @@
                     </div>
 
                     <!-- Right column -->
-                    <div class="container__activities col col-md-9">
+                    <div class="container__tab-content col col-md-9">
                         <!-- Footprint & Wishlist navigation -->
                         <nav class="activities-nav">
                             <h2 class="hidden">Footprint and Wish list navigation</h2>
                             <ul class="nav nav-pills">
-                                <li class="active"><a data-toggle="pill" href="#footprints"><span class="glyphicon glyphicon-road"></span>Footprint</a></li>
-                                <li><a data-toggle="pill" href="#wishlist"><span class="glyphicon glyphicon-eye-open"></span>Wishlist</a></li>
+                                <li class="active"><a data-toggle="tab" href="#footprints"><span class="glyphicon glyphicon-road"></span>Footprint</a></li>
+                                <li><a data-toggle="tab" href="#wishlist"><span class="glyphicon glyphicon-eye-open"></span>Wishlist</a></li>
                             </ul>
                         </nav>
 
-                        <div class="tab-content">
+                        <div class="tab-content clearfix">
                             <!-- Tab: Footprints -->
                             <div id="footprints" class="tab-pane fade in active">
                                 <!-- Past Footprints -->
@@ -78,7 +121,7 @@
                                     </div>
                                 </div>
 
-                                <div class="footprint">
+                                <div id="1" class="footprint display-group">
                                     <div class="row">
                                         <div class="col col-xs-1 col-lg-1"><img src="../static/img/users/profile/1.png" /></div>
                                         <div class="col col-xs-1 col-lg-11">
@@ -95,7 +138,7 @@
                                     </div>
                                 </div>
 
-                                <div class="footprint">
+                                <div id="2" class="footprint display-group">
                                     <div class="row">
                                         <div class="col col-xs-1 col-lg-1"><img src="../static/img/users/profile/1.png" /></div>
                                         <div class="col col-xs-1 col-lg-11">
@@ -112,7 +155,7 @@
                                     </div>
                                 </div>
 
-                                <div class="footprint">
+                                <div id="3" class="footprint display-group">
                                     <div class="row">
                                         <div class="col col-xs-1 col-lg-1"><img src="../static/img/users/profile/1.png" /></div>
                                         <div class="col col-xs-1 col-lg-11">
@@ -131,10 +174,53 @@
 
                             </div>
 
-                            <!-- Tab: Wishlist -->
-                            <div id="wishlist" class="tab=pane fade">
-                                <h2>Your Wish list</h2>
-                                <div>Your wish list will appear here ...</div>
+                            <!-- Tab: Wishlist-->
+                            <div id="wishlist" class="tab-pane fade">
+
+                                <div id="w1" class="display-group">
+                                    <div class="row">
+                                        <div class="col col-xs-4 col-lg-4 wishlist-group__thumbnail">
+                                            <img src="../static/img/park/0/0.jpg" alt="Park picture" />
+                                        </div>
+                                        <div class="col col-xs-8 col-lg-8 wishlist-group__park-details">
+                                            <div>
+                                                <a class="wishlist-group__park-link" href="" alt="Link to park profile page">[Nahanni National Park Reserve of Canada]</a>
+                                            </div>
+                                            <div class="wishlist-group__more-details">[Fort Smith, Unorganized, NT X0E, Canada]</div>
+                                            <div class="wishlist-group__more-details">[Northwest Territories]</div>
+                                        </div>
+                                    </div>
+                                    <div class="row wishlist-group__footer">
+                                        <div class="col col-xs-12 col-lg-12">
+                                            <span class="wishlist-group__more-details">Added on [Feb 11, 2017]</span>
+                                            &nbsp;|&nbsp;
+                                            <span><a href="" alt="Link to remove park from wishlist">Remove</a></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div id="w2" class="display-group">
+                                    <div class="row">
+                                        <div class="col col-xs-4 col-lg-4 wishlist-group__thumbnail">
+                                            <img src="../static/img/park/0/0.jpg" alt="Park picture" />
+                                        </div>
+                                        <div class="col col-xs-8 col-lg-8 wishlist-group__park-details">
+                                            <div>
+                                                <a class="wishlist-group__park-link" href="" alt="Link to park profile page">[Prince Edward Island National Park]</a>
+                                            </div>
+                                            <div class="wishlist-group__park-address">[North Rustico, PE C0A, Canada]</div>
+                                            <div class="wishlist-group__park-province">[Prince Edward Island]</div>
+                                        </div>
+                                    </div>
+                                    <div class="row wishlist-group__footer">
+                                        <div class="col col-xs-12 col-lg-12">
+                                            <span>Added on [Jan 29, 2017]</span>
+                                            &nbsp;|&nbsp;
+                                            <span><a href="" alt="Link to remove park from wishlist">Remove</a></span>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>  <!-- end of right column div -->
@@ -143,28 +229,68 @@
             </main>
 
             <!-- Modal Window -->
+            <!-- ------------ -->
             <div class="modal fade bs-example-modal-lg" id="myNewFootprint" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
+                        <!-- Modal Header -->
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                             <h3 class="modal-title" id="myModalLabel">Share a New Footprint Event</h3>
                         </div>
+                        <!-- Modal Body -->
                         <div class="modal-body">
                             <div class="row">
-                                <div class="col-md-12">
-                                    Travelled Today
+                                <div class="col-md-12 new-footprint-header">
+                                    <div class="new-footprint__icon"><span class="glyphicon glyphicon-leaf"></span></div>
+                                    <div class="new-footprint__text">Has been on a new adventure</div>
+                                    <div class="new-footprint__today">Today</div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    Form collecting data about new footprint ...
-                                </div>
-                                <div class="col-md-6">
-                                    Section to allow for images upload ...
-                                </div>
+                            <div class="row new-footprint-data">
+                                <form name="" action="" method="post" enctype="multipart/form-data">
+                                    <!-- TODO: Form to add a new footprint -->
+                                    <div class="col-md-6 new-footprint-form">
+                                            <div class="form-group row">
+                                                <label for="slctPark" class="col-sm-5 col-form-label">Park visited</label>
+                                                <div class="col-sm-7">
+                                                    <select id="slctPark" name="parkVisited" class="form-control">
+                                                        <?php foreach ($lstParks as $parkDetails) {
+                                                            echo "<option value=\"{$parkDetails['id']}\">{$parkDetails['name']}</option>";
+                                                        } ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="inputDateVisit" class="col-sm-5 col-form-label">Date visited</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" class="form-control" id="inputDateVisit" placeholder="Date visited park" />
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="inputStory" class="col-sm-5 col-form-label">Story</label>
+                                                <div class="col-sm-7">
+                                                    <textarea class="form-control" id="inputStory" row="4" placeholder="Optional"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="isPublic" class="col-sm-5 col-form-label">Share post as public?</label>
+                                                <div class="col-sm-7">
+                                                    <input class="form-check-input" type="checkbox" id="isPublic" value="Y">
+                                                    <label for="isPublic">Yes</label>
+                                                </div>
+                                            </div>
+                                    </div>
+                                    <div class="col-md-6 new-footprint-images">
+                                        <label for="yourImages">Upload your pictures</label>
+                                        <input type="file" id="yourImages" name="files[]" multiple="multiple" accept="image/*" />
+                                    </div>
+                                </form>
                             </div>
                         </div>
+                        <!-- Modal Footer -->
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary">Share</button>
                             <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
@@ -176,6 +302,6 @@
             <!-- Page Footer -->
             <?php include_once "../templates/footer.php" ?>
         </div>
-            <!-- <script type="text/javascript" src="compare.js"></script>-->
+        <script type="text/javascript" src="../static/js/profile.js"></script>
     </body>
 </html>
